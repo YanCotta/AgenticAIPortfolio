849a93b534d6230d617840519a383e5bd2e87e16
Yan Cotta <yanpcotta@gmail.com>
2025-02-08 11:02:38 -0300

    Removed IPython dependency.

--- AutoGen/FinancialAnalysisAndCoding/MultiAgentFinancialAnalysis/main.py
+++ AutoGen/FinancialAnalysisAndCoding/MultiAgentFinancialAnalysis/main.py
@@ -3,7 +3,8 @@
 
 import datetime
 import os
-from IPython.display import Image
+# from IPython.display import Image # REMOVE
+import matplotlib.pyplot as plt  # ADD
 
 from autogen import ConversableAgent, AssistantAgent
 
@@ -28,7 +29,9 @@ Make sure the code is in markdown code block and save the figure to a file ytd_s
         message=message1,
     )
     
-    Image(os.path.join(settings.work_dir, "ytd_stock_gains.png"))
+    # Image(os.path.join(settings.work_dir, "ytd_stock_gains.png")) # REMOVE
+    image_path1 = os.path.join(settings.work_dir, "ytd_stock_gains.png")  # ADD
+    print(f"Plot saved to: {image_path1}")  # ADD
     
     message2 = f"""Today is {today}.
 Download the stock prices YTD for NVDA and TSLA and create a plot.
@@ -39,7 +42,9 @@ Make sure the code is in markdown code block and save the figure to a file stock
         message=message2,
     )
     
-    Image(os.path.join(settings.work_dir, "stock_prices_YTD_plot.png"))
+    # Image(os.path.join(settings.work_dir, "stock_prices_YTD_plot.png")) # REMOVE
+    image_path2 = os.path.join(settings.work_dir, "stock_prices_YTD_plot.png")  # ADD
+    print(f"Plot saved to: {image_path2}")  # ADD
 
 if __name__ == '__main__':
     main()
